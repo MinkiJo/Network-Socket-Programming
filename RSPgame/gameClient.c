474cb1c7d7eb20569d835315d9d760cf1a5173f4
조민기 <62167266+MinkiJo@users.noreply.github.com>
2020-11-05 21:08:28 +0900

    Update gameClient.c

--- RSPgame/gameClient.c
+++ RSPgame/gameClient.c
@@ -1,24 +1,13 @@
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
-
 #include <netdb.h>
-
- 
-
 #include <stdio.h>
-
 #include <string.h>
 #include<stdlib.h>
 
- 
-
 #define closesocket      close
-
 #define PROTOPORT        5193        /* default protocol port number */
 
  
